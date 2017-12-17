80ae2e0e968d82393d6578ecb19b28ea5c49d3de
柴田豊 <shibatayutaka@shibataUTnoMacBook-Air.local>
2017-12-17 17:04:38 +0900

    ツイカ

--- src/main/java/jp/jleague/club/api/facade/v1/sample/SampleFacade.java
+++
@@ -1,5 +0,0 @@
-package jp.jleague.club.api.facade.v1.sample;
-
-public interface SampleFacade {
-
-}
--- src/main/java/jp/jleague/club/api/facade/v1/sample/impl/SampleFacadeImpl.java
+++
@@ -1,7 +0,0 @@
-package jp.jleague.club.api.facade.v1.sample.impl;
-
-import jp.jleague.club.api.facade.v1.sample.SampleFacade;
-
-public class SampleFacadeImpl implements SampleFacade{
-
-}
