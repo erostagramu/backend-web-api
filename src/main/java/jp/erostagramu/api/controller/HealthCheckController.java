3a7becf6d777a0352ffd09f2c9eed1195c6850bf
Takuma Okamatsu <standlife1993@gmail.com>
2017-12-24 16:04:08 +0900

    ビルド失敗2

---
+++ src/main/java/jp/erostagramu/api/controller/HealthCheckController.java
@@ -0,0 +1,10 @@
+package jp.erostagramu.api.controller;
+
+@RestController
+@RequestMapping(value = "api")
+
+public class HealthCheckController {
+	@RequestMapping(value = "/v1/movie", method = RequestMethod.GET)
+	public 
+	
+}
