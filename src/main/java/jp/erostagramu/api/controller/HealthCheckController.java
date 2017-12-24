bc2cff16cfc72de32109567a31f9c5eb64442595
Takuma Okamatsu <standlife1993@gmail.com>
2017-12-25 01:23:48 +0900

    ヘルスチェックAPI修正_1

--- src/main/java/jp/erostagramu/api/controller/HealthCheckController.java
+++ src/main/java/jp/erostagramu/api/controller/HealthCheckController.java
@@ -1,48 +1,38 @@
 package jp.erostagramu.api.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "api")
+@RequestMapping(value = "api/v1/health_check")
 public class HealthCheckController {
 
-	@RequestMapping(value = "/v1/movie/{page}/{categoryId}/{searchStr}", method = RequestMethod.GET)
-	public Map<String, String> get(@PathVariable("page") Integer page, @PathVariable("categoryId") Integer categoryId,
-			@PathVariable("searchStr") String searchStr) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("メソッド", "get");
-		map.put("ページ", page.toString());
-		map.put("カテゴリId", categoryId.toString());
-		map.put("検索ワード", searchStr);
-		return map;
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public HealthCheckInfoDto get() {
+		HealthCheckInfoDto dto = new HealthCheckInfoDto();
+		dto.setStatus("success");
+		return dto;
 	}
 
-	@RequestMapping(value = "/v1/movie", method = RequestMethod.POST)
-	public Map<String, String> post() {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("メソッド", "post");
-		return map;
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public HealthCheckInfoDto post() {
+		HealthCheckInfoDto dto = new HealthCheckInfoDto();
+		dto.setStatus("success");
+		return dto;
 	}
 
-	@RequestMapping(value = "/v1/movie/{id}", method = RequestMethod.PUT)
-	public Map<String, String> put(@PathVariable("id") Integer id) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("メソッド", "put");
-		map.put("id", id.toString());
-		return map;
+	@RequestMapping(value = "/", method = RequestMethod.PUT)
+	public HealthCheckInfoDto put() {
+		HealthCheckInfoDto dto = new HealthCheckInfoDto();
+		dto.setStatus("success");
+		return dto;
 	}
 
-	@RequestMapping(value = "/v1/movie/{id}", method = RequestMethod.DELETE)
-	public Map<String, String> delete(@PathVariable("id") Integer id) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("メソッド", "delete");
-		map.put("id", id.toString());
-		return map;
+	@RequestMapping(value = "/", method = RequestMethod.DELETE)
+	public HealthCheckInfoDto delete() {
+		HealthCheckInfoDto dto = new HealthCheckInfoDto();
+		dto.setStatus("success");
+		return dto;
 	}
 }
---
+++ src/main/java/jp/erostagramu/api/controller/HealthCheckInfoDto.java
@@ -0,0 +1,15 @@
+package jp.erostagramu.api.controller;
+
+public class HealthCheckInfoDto {
+
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
