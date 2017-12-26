71f4e3ae99062b198c67408d998568c0feaf0047
Takuma Okamatsu <standlife1993@gmail.com>
2017-12-26 23:33:46 +0900

    Transactionをクラスに設定

--- src/main/java/jp/erostagramu/api/controller/HealthCheckController.java
+++ src/main/java/jp/erostagramu/api/controller/HealthCheckController.java
@@ -5,31 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Transactional(timeout = 15)
 @RestController
 @RequestMapping(value = "api/v1/healthCheck")
 public class HealthCheckController {
 
 	private static final String SUCCESS_STATUS = "success";
-	
-	@Transactional(timeout = 15)
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public HealthCheckInfoDto validateGetMethod() {
 		return HealthCheckInfoDto.builder().status(SUCCESS_STATUS).build();
 	}
-	
-	@Transactional(timeout = 15)
+
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public HealthCheckInfoDto validatePostMethod() {
 		return HealthCheckInfoDto.builder().status(SUCCESS_STATUS).build();
 	}
 
-	@Transactional(timeout = 15)
 	@RequestMapping(value = "/", method = RequestMethod.PUT)
 	public HealthCheckInfoDto validatePutMethod() {
 		return HealthCheckInfoDto.builder().status(SUCCESS_STATUS).build();
 	}
-	
-	@Transactional(timeout = 15)
+
 	@RequestMapping(value = "/", method = RequestMethod.DELETE)
 	public HealthCheckInfoDto validateDeleteMethod() {
 		return HealthCheckInfoDto.builder().status(SUCCESS_STATUS).build();
