06e2719643fed5a0e84f025cdd5da8799764d410
Takuma Okamatsu <standlife1993@gmail.com>
2018-01-01 23:41:22 +0900

    MyBatis環境構築_3

---
+++ src/main/java/jp/erostagramu/api/controller/TestController.java
@@ -0,0 +1,43 @@
+package jp.erostagramu.api.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jp.erostagramu.api.dto.MovieDto;
+
+@Controller
+@EnableAutoConfiguration
+public class TestController {
+	
+	@RequestMapping("/service")
+	@ResponseBody
+	public List<String> top() {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream;
+		MovieDto md = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession session = sqlSessionFactory.openSession();
+			md = session.selectOne("jp.erostagramu.api.mybatis.mapper.TestMapper.selectTest", 1000);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		List<String> list = new ArrayList<String>();
+		list.add("太宰");
+		list.add("夏目");
+		list.add(md.getTitle());
+		return list;
+	}
+}
--- src/main/java/jp/erostagramu/api/mybatis/mapper/TestMapper.java
+++ src/main/java/jp/erostagramu/api/mybatis/mapper/TestMapper.java
@@ -3,7 +3,5 @@ package jp.erostagramu.api.mybatis.mapper;
 import jp.erostagramu.api.dto.MovieDto;
 
 public interface TestMapper {
-
-	MovieDto selectTest(int id);
-
-}
+	    MovieDto selectTest(int id);
+	}
