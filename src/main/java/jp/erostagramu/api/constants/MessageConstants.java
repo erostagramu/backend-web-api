cfceef9352fb9a10944702ba4addd058f0734a06
Takuma Okamatsu <standlife1993@gmail.com>
2018-03-04 16:43:36 +0900

    エラーメッセージを定数クラスに分離

---
+++ src/main/java/jp/erostagramu/api/constants/MessageConstants.java
@@ -0,0 +1,13 @@
+package jp.erostagramu.api.constants;
+
+public class MessageConstants {
+	
+	// 成功時のメッセージ
+	public static final String SUCCESS = "成功";
+	
+	// エラー時のメッセージ
+	public static final String ID_DUPLICATION = "[id]が重複しています";
+	public static final String CATEGORY_ID_INTEGRITY = "存在しない[categoryId]を指定しています";
+	public static final String OTHER = "不明なエラー";
+	
+}
--- src/main/java/jp/erostagramu/api/logic/v1/create/impl/CreateLogicImpl.java
+++ src/main/java/jp/erostagramu/api/logic/v1/create/impl/CreateLogicImpl.java
@@ -1,10 +1,12 @@
 package jp.erostagramu.api.logic.v1.create.impl;
 
-import org.apache.ibatis.exceptions.PersistenceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import jp.erostagramu.api.constants.MessageConstants;
 import jp.erostagramu.api.dao.masterdb.dto.ResultDto;
 import jp.erostagramu.api.dao.masterdb.write.CreatePostDao;
 import jp.erostagramu.api.facade.v1.crud.model.CreatePostFacadeRequest;
@@ -17,23 +19,32 @@ public class CreateLogicImpl implements CreateLogic {
 	@Autowired
 	private CreatePostDao createDao;
 	private ResultDto result;
-	private HttpStatus status;
 	private String message;
+	private HttpStatus status;
 
 	@Override
 	public CreatePostFacadeResponse create(CreatePostFacadeRequest createPostFacadeRequest) {
 
 		try {
 			createDao.create(createPostFacadeRequest);
-			message = "成功";
+
+			// リクエスト成功時
+			message = MessageConstants.SUCCESS;
 			status = HttpStatus.OK;
 
-		} catch (PersistenceException e) {
-			message = "'id'または'categoryId'が重複しています";
+		} catch (DuplicateKeyException e) {
+			// 既に存在するIDを指定した時
+			message = MessageConstants.ID_DUPLICATION;
 			status = HttpStatus.CONFLICT;
-			
+
+		} catch (DataIntegrityViolationException e) {
+			// 存在しないcategoryIDを指定した時
+			message = MessageConstants.CATEGORY_ID_INTEGRITY;
+			status = HttpStatus.BAD_REQUEST;
+
 		} catch (Exception e) {
-			message = "不明なエラー";
+			// その他の問題が生じた時
+			message = MessageConstants.OTHER;
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
 			e.printStackTrace();
 		}
